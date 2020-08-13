2f2e0968b8c0f0bde6204656ac024baa603d1095
Luis Enrique <luyo.lemp@gmail.com>
2020-08-12 22:45:15 -0300

    added fake api to render some pokemons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler'
+import { Text, View, TextInput, FlatList, Image } from 'react-native'
 import { Ionicons } from "@expo/vector-icons";
 import { useFonts,  Roboto_700Bold, Roboto_400Regular } from "@expo-google-fonts/roboto";
 import { AppLoading } from 'expo';
 
 import styles from './styles'
+
+const DATA = [
+  {
+    id: 1,
+    name: 'Bulbassaur',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+  },
+  {
+    id: 2,
+    name: 'charmander',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
+  }
+]
 
 export default function Home() {
   let [fontLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
@@ -31,6 +43,20 @@ export default function Home() {
         <Ionicons style={styles.searchIcon} name="ios-options" size={20} color="#000"/>
       </View>
       <Text>The Pokédex contains detailed stats for every creature from the Pokémon games.</Text>
+
+      <FlatList
+        style={{width: "90%", height: 'auto'}}
+        data={DATA}
+        keyExtractor={ item => item.id}
+        horizontal={false}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+          <View>
+            <Image source={{ uri: item.img }} style={{width: 100, height: 40}}/>
+            <Text>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
   )}
 }
